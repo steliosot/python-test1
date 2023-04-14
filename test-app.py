19e37759023175845d1fc66e26a343d4e6e9b9f2
steliosot <steliosot@msn.com>
2023-04-14 12:08:05 +0100

    init commit

---
+++ test-app.py
@@ -0,0 +1,16 @@
+import pytest
+
+from app import addTwo
+
+def test_add_positive():
+  assert addTwo(1,2) == 3
+
+def test_add_zero():
+  assert addTwo(0,0) == 0
+
+def test_negative():
+  assert addTwo(10,-3) == 7
+
+def addTwo_string__expect_exception():
+  with pytest.raises(TypeError):
+    addTwo(10,'a')
